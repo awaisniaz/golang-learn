e0bbe32e1a009d35fa62f4e5e0b5c2c9e7a44784
awais-codora <awais.niaz@codora.io>
2025-02-06 23:12:57 +0500

    fix: learn goroutine

--- channel.go
+++ channel.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func Channel() {
@@ -12,21 +11,27 @@ func Channel() {
 	fmt.Println(val)
 }
 func main() {
-	ch := make(chan int)
-	fmt.Println("Sending value to channel")
-	go send(ch)
+	// ch := make(chan int)
+	// fmt.Println("Sending value to channel")
+	// go send(ch)
 
-	fmt.Println("Receiving from channel")
-	go receiver(ch)
+	// fmt.Println("Receiving from channel")
+	// go receiver(ch)
 
-	time.Sleep(time.Second * 1)
+	// time.Sleep(time.Second * 1)
+	fmt.Print("I am Awaus Buaz")
+	go parallism()
+	fmt.Printf("I am End position")
 
+}
+
+func parallism() {
+	fmt.Printf("I am Goroutine")
 }
 
 func send(ch chan int) {
 	ch <- 1
 }
-
 func receiver(ch chan int) {
 	val := <-ch
 	fmt.Printf("Value Received=%d in receive function\n", val)
